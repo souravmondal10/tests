242e5eef453188ab4c1cbf1ab2b232505f15e78c
sourav mondal <souravmondal10@gmail.com>
2021-06-22 05:38:02 +0000

    updated code

---
+++ config-sample.php
@@ -0,0 +1,9 @@
+<?php
+//sample file
+define('REDIS_HOST', 'redis_host_xxx');
+define('REDIS_PORT', redis_port_xxx);
+define('MYSQL_HOST', 'mysql_host_xxx');
+define('MYSQL_PORT', 'mysql_port_xxx');
+define('MYSQL_DATABASE', 'mysql_database_xxx');
+define('MYSQL_USER', 'mysql_user_xxx');
+define('MYSQL_PASSWORD', 'mysql_password_xxx');
